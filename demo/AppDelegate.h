b53d1b7e1fc0ee8002b07955df2b5ebc3518d05e
zengxianshu <768634529@qq.com>
2015-04-17 13:08:25 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 曾宪书 on 15/4/17.
+//  Copyright (c) 2015年 名兵团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
