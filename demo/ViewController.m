105a87857f23f952fcd286400ac8b9ab7721cd96
zengxianshu <768634529@qq.com>
2015-08-25 18:10:15 +0800

    本地通知
    建议使用Block

--- demo/ViewController.m
+++ demo/ViewController.m
@@ -16,7 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //注册通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(action:) name:@"123" object:nil];
+    
+    NSString *str=@"1234567";
+    
+    NSDictionary *dic=[[NSDictionary alloc]initWithObjectsAndKeys:str,@"and", nil];
+    //发送通知
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:@"123"
+     object:self userInfo:dic];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+-(void)action:(NSNotification *)notification
+{
+    NSLog(@"%@",notification.userInfo);
 }
 
 - (void)didReceiveMemoryWarning {
