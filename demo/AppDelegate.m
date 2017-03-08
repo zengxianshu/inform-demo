7dcac479a38c336ec2a844922298bfea8aa31886
zengXianShu <zengxiangshu0@163.com>
2017-03-08 16:34:07 +0800

    commit

--- demo/AppDelegate.m
+++ demo/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  demo
 //
-//  Created by 曾宪书 on 15/4/17.
-//  Copyright (c) 2015年 名兵团. All rights reserved.
+//  Created by 曾墨 on 15/4/17.
+//  Copyright (c) 2015年 . All rights reserved.
 //
 
 #import "AppDelegate.h"
--- demo/ViewController.m
+++ demo/ViewController.m
@@ -1,9 +1,9 @@
 //
-//  ViewController.m
-//  demo
+//  FuzzyBoundariesView.swift
+//  Onion
 //
-//  Created by 曾宪书 on 15/4/17.
-//  Copyright (c) 2015年 名兵团. All rights reserved.
+//  Created by 曾墨 on 15/4/17.
+//  Copyright (c) 2015年 . All rights reserved.
 //
 
 #import "ViewController.h"
